d4653af7c882d2e7675098ab48c449b8474214ec
Katerina Dietkova <katrinkir1296@gmail.com>
2022-11-22 08:49:53 +0200

    add js scripts

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './js/header-scroll';
+import './js/inputmask';
+import './js/mobile-menu';
+import './js/modal';
--- src/js/inputmask.js
+++ src/js/inputmask.js
@@ -1,3 +1,5 @@
+import Inputmask from 'inputmask';
+
 const selector = document.getElementById('user-phone');
 
 const im = new Inputmask('+38 (099) 999 99 99');
